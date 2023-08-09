e89e87f90427c0ee65037620e1e4cf3e71b93f87
Bayu Putra <bayuputra18@protonmail.com>
2023-08-09 12:06:07 +0700

    nginx load balance

--- Nginx/install.sh
+++ Nginx/install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 apt update
-apt install apt-transport-https ca-certificates curl software-properties-common
+apt install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
-sudo apt install docker-ce docker-ce-cli containerd.io
+sudo apt install -y docker-ce docker-ce-cli containerd.io
 sudo systemctl start docker
 sudo systemctl enable docker
 mkdir docker
